dc626b39e3c5debe680c550aa53650564d4af823
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-12 12:47:44 -0600

    How do dates work lol.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,28 @@
-<p> Hello, World! </p>
+<script>
+  const future = new Date('2019-05-29T14:00:00-0600');
+  let now = new Date();
+  let timedelta = future - now;
+
+  let leftover = timedelta;
+  let days = ~~(leftover / (24 * 60 * 60 * 1000));
+  leftover = ~~(leftover % (24 * 60 * 60 * 1000));
+
+  let hours = ~~(leftover / (60 * 60 * 1000));
+  leftover = ~~(leftover % (60 * 60 * 1000));
+
+  let minutes = ~~(leftover / (60 * 1000));
+  leftover = ~~(leftover % (60 * 1000));
+
+  let seconds = ~~(leftover / 1000);
+</script>
+
+
+<main>
+  <ul>
+    <li> {days} days
+    <li> {hours} hours
+    <li> {minutes} minutes
+    <li> {seconds} seconds
+  </ul>
+</main>
+<!-- vim: ft=html ts=2 sw=2: -->
