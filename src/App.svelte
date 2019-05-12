af4a62bfe8c96c476c1636a65988d6fca70be153
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-12 15:22:45 -0600

    why polish...

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
 <script>
   const future = new Date('2019-05-29T14:00:00-0600');
+  const locale = 'pl-PL';
+
+  let asNum = (function(inst) {
+    return inst.format.bind(inst);
+  }(new Intl.NumberFormat(locale, {
+    style: 'decimal',
+    useGrouping: true
+  })));
+
+  const dictionary = {
+    'en-CA': {
+      'day': {one: 'day', other: 'days'},
+      'hour': {one: 'hour', other: 'hours'},
+      'minute': {one: 'minute', other: 'minutes'},
+      'second': {one: 'second', other: 'seconds'},
+    },
+    'es-GT': {
+      'day': {one: 'dia', other: 'dias'},
+      'hour': {one: 'hora', other: 'horas'},
+      'minute': {one: 'minuto', other: 'minutos'},
+      'second': {one: 'segundo', other: 'segundos'},
+    },
+    'pl-PL': {
+      'day': {one: 'dziń', few: 'dni', many: 'dni'},
+      'hour': {one: 'godzina', few: 'godziny', many: 'godzin'},
+      'minute': {one: 'minuta', few: 'minuty', many: 'minut'},
+      'second': {one: 'secunda', few: 'secundy', many: 'secund'},
+    },
+  }
+
   let now = new Date();
   let timedelta = future - now;
 
@@ -14,15 +44,22 @@
   leftover = ~~(leftover % (60 * 1000));
 
   let seconds = ~~(leftover / 1000);
+
+let pluralRules = new Intl.PluralRules(locale, { type: 'cardinal' });
+  function pluralize(num, key) {
+    let plural = pluralRules.select(num);
+    let text = dictionary[locale][key][plural];
+    return `${asNum(num)} ${text}`;
+  }
 </script>
 
 
 <main>
   <ul>
-    <li> {days} days
-    <li> {hours} hours
-    <li> {minutes} minutes
-    <li> {seconds} seconds
+    <li> {pluralize(days, 'day')}
+    <li> {pluralize(hours, 'hour')}
+    <li> {pluralize(minutes, 'minute')}
+    <li> {pluralize(seconds, 'second')}
   </ul>
 </main>
 <!-- vim: ft=html ts=2 sw=2: -->
