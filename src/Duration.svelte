9664e883cf008c914044fdbb577d8a1d70c67fdc
Eddie Antonio Santos <easantos@ualberta.ca>
2019-05-12 19:02:46 -0600

    Split dictionary into its own file.

--- src/Duration.svelte
+++ src/Duration.svelte
@@ -3,26 +3,7 @@
   export let unit;
   export let locale;
 
-  const dictionary = {
-    'en-CA': {
-      'day': {one: 'day', other: 'days'},
-      'hour': {one: 'hour', other: 'hours'},
-      'minute': {one: 'minute', other: 'minutes'},
-      'second': {one: 'second', other: 'seconds'},
-    },
-    'es-GT': {
-      'day': {one: 'dia', other: 'dias'},
-      'hour': {one: 'hora', other: 'horas'},
-      'minute': {one: 'minuto', other: 'minutos'},
-      'second': {one: 'segundo', other: 'segundos'},
-    },
-    'pl-PL': {
-      'day': {one: 'dziń', few: 'dni', many: 'dni', other: 'dni'},
-      'hour': {one: 'godzina', few: 'godziny', many: 'godzin', other: 'godzin'},
-      'minute': {one: 'minuta', few: 'minuty', many: 'minut', other: 'minut'},
-      'second': {one: 'secunda', few: 'secundy', many: 'secund', other: 'secund'},
-    },
-  };
+  import dictionary from './dictionary';
 
   let asNum = (function(inst) {
     return inst.format.bind(inst);
---
+++ src/dictionary.js
@@ -0,0 +1,20 @@
+export default {
+  'en-CA': {
+    'day': {one: 'day', other: 'days'},
+    'hour': {one: 'hour', other: 'hours'},
+    'minute': {one: 'minute', other: 'minutes'},
+    'second': {one: 'second', other: 'seconds'},
+  },
+  'es-GT': {
+    'day': {one: 'dia', other: 'dias'},
+    'hour': {one: 'hora', other: 'horas'},
+    'minute': {one: 'minuto', other: 'minutos'},
+    'second': {one: 'segundo', other: 'segundos'},
+  },
+  'pl-PL': {
+    'day': {one: 'dziń', few: 'dni', many: 'dni', other: 'dni'},
+    'hour': {one: 'godzina', few: 'godziny', many: 'godzin', other: 'godzin'},
+    'minute': {one: 'minuta', few: 'minuty', many: 'minut', other: 'minut'},
+    'second': {one: 'secunda', few: 'secundy', many: 'secund', other: 'secund'},
+  },
+};
